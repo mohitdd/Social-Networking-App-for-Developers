1898756f8d082b0c8f08c511edf9fab2e31b4213
Mohit Dhingra <mohit.dhingra@emc.com>
2019-07-12 02:18:48 +0530

    Feature to add/delete the user profile

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -125,4 +125,107 @@ router.put("/like/:id", auth, async (req, res) => {
   }
 });
 
+//@route    PUT api/posts/unlike/:id
+//@desc     Unlike a Post
+//@access   private
+
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //check wheather the post is already liked or not
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).send("Post is not liked by the user");
+    }
+
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    res.send("Post is unliked by the user");
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route    PUT api/posts/comment/:id
+//@desc     Comment on a post
+//@access   Private
+
+router.put(
+  "/comment/:id",
+  [
+    auth,
+    [
+      check("text", "Text field is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ error: errors.array() });
+    }
+
+    const user = await User.findById(req.user.id).select("-password");
+    const post = await Post.findById(req.params.id);
+
+    const newComment = {
+      user: req.user.id,
+      text: req.body.text,
+      avatar: user.avatar,
+      name: user.name
+    };
+
+    post.comments.unshift(newComment);
+    await post.save();
+    res.json(post);
+  }
+);
+
+// @route    DELETE api/posts/comment/:id/:comment_id
+// @desc     Delete comment
+// @access   Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments
+      .map(comment => comment.id)
+      .indexOf(req.params.comment_id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
